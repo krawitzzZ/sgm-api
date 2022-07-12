81fb743d4fcece5d92264b77275d7565ff1844e9
Nikita Demin <nikita.demin@franka.de>
2022-07-12 20:57:28 +0200

    update devcontainer

--- src/Infra/EventRepository.hs
+++ src/Infra/EventRepository.hs
@@ -52,8 +52,7 @@ import           RIO                                                ( ($)
 
 -- TODO get by sorting, time, etc
 getAll :: (Has Connection e, Has Config e, MonadCatch m, MonadIO m) => e -> m [Event]
-getAll c = do
-  tryCatchBeamDefault $ allEvents c <&> map (uncurry eventEntityToDomain)
+getAll c = tryCatchBeamDefault $ allEvents c <&> map (uncurry eventEntityToDomain)
 
 findOneById :: (Has Connection e, Has Config e, MonadCatch m, MonadIO m) => e -> EventId -> m Event
 findOneById e eventId = tryCatchBeamDefault $ maybeEventById e eventId >>= \case
