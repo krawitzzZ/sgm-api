8ed029e12f0bb8771c9aa5af6797c71c52a56445
Nikita Demin <nikita.demin@franka.de>
2022-07-14 23:04:00 +0200

    update container install script to start ssh-agent

--- .devcontainer/library-scripts/common-debian.sh
+++ .devcontainer/library-scripts/common-debian.sh
@@ -257,6 +257,16 @@ if [ -z "$(git config --get core.editor)" ] && [ -z "${GIT_EDITOR}" ]; then
     fi
 fi
 
+# autostart ssh-agent
+if [ -z "$SSH_AUTH_SOCK" ]; then
+   # Check for a currently running instance of the agent
+   RUNNING_AGENT="`ps -ax | grep 'ssh-agent -s' | grep -v grep | wc -l | tr -d '[:space:]'`"
+   if [ "$RUNNING_AGENT" = "0" ]; then
+        # Launch a new instance of the agent
+        ssh-agent -s &> $HOME/.ssh/ssh-agent
+   fi
+   eval `cat $HOME/.ssh/ssh-agent`
+fi
 EOF
 )"
 
