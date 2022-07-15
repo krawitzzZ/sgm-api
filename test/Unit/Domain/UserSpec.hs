db8d6537904a70acc1fcafcaed6c8492f072683d
Nikita Demin <nikita.demin@franka.de>
2022-07-15 11:40:26 +0200

    adjust UserSpec description

--- test/Unit/Domain/UserSpec.hs
+++ test/Unit/Domain/UserSpec.hs
@@ -23,66 +23,65 @@ import           Test.Hspec.QuickCheck                              ( prop )
 
 spec :: Spec
 spec = parallel $ do
-  describe "User" $ do
-    describe "AccessPolicy instance" $ do
+  describe "AccessPolicy instance" $ do
 
-      context "when user is Superadmin" $ do
-        it "should be able to create new users" $ do
-          createUser superadminClaims `shouldBe` Granted
-        prop "should be able to update any user"
-          $ \uid -> updateUser superadminClaims uid `shouldBe` Granted
-        prop "should be able to delete any user"
-          $ \uid -> deleteUser superadminClaims uid `shouldBe` Granted
-        it "should be able to get all users" $ do
-          getUsers superadminClaims `shouldBe` Granted
-        it "should be able to get a user" $ do
-          getUser superadminClaims `shouldBe` Granted
+    context "when user is Superadmin" $ do
+      it "should be able to create new users" $ do
+        createUser superadminClaims `shouldBe` Granted
+      prop "should be able to update any user"
+        $ \uid -> updateUser superadminClaims uid `shouldBe` Granted
+      prop "should be able to delete any user"
+        $ \uid -> deleteUser superadminClaims uid `shouldBe` Granted
+      it "should be able to get all users" $ do
+        getUsers superadminClaims `shouldBe` Granted
+      it "should be able to get a user" $ do
+        getUser superadminClaims `shouldBe` Granted
 
-      context "when user is Admin" $ do
-        it "should not be able to create new users" $ do
-          createUser adminClaims `shouldBe` Denied
-        prop "should be able to update any user"
-          $ \uid -> updateUser adminClaims uid `shouldBe` Granted
-        prop "should not be able to delete any user"
-          $ \uid -> deleteUser adminClaims uid `shouldBe` Denied
-        it "should only be able to delete himself" $ do
-          deleteUser adminClaims (ucId adminClaims) `shouldBe` Granted
-        it "should be able to get all users" $ do
-          getUsers adminClaims `shouldBe` Granted
-        it "should be able to get a user" $ do
-          getUser adminClaims `shouldBe` Granted
+    context "when user is Admin" $ do
+      it "should not be able to create new users" $ do
+        createUser adminClaims `shouldBe` Denied
+      prop "should be able to update any user"
+        $ \uid -> updateUser adminClaims uid `shouldBe` Granted
+      prop "should not be able to delete any user"
+        $ \uid -> deleteUser adminClaims uid `shouldBe` Denied
+      it "should only be able to delete himself" $ do
+        deleteUser adminClaims (ucId adminClaims) `shouldBe` Granted
+      it "should be able to get all users" $ do
+        getUsers adminClaims `shouldBe` Granted
+      it "should be able to get a user" $ do
+        getUser adminClaims `shouldBe` Granted
 
-      context "when user is Moderator" $ do
-        it "should not be able to create new users" $ do
-          createUser moderatorClaims `shouldBe` Denied
-        prop "should not be able to update any user"
-          $ \uid -> updateUser moderatorClaims uid `shouldBe` Denied
-        it "should only be able to update himself" $ do
-          updateUser moderatorClaims (ucId moderatorClaims) `shouldBe` Granted
-        prop "should not be able to delete any user"
-          $ \uid -> deleteUser moderatorClaims uid `shouldBe` Denied
-        it "should only be able to delete himself" $ do
-          deleteUser moderatorClaims (ucId moderatorClaims) `shouldBe` Granted
-        it "should be able to get all users" $ do
-          getUsers moderatorClaims `shouldBe` Granted
-        it "should be able to get a user" $ do
-          getUser moderatorClaims `shouldBe` Granted
+    context "when user is Moderator" $ do
+      it "should not be able to create new users" $ do
+        createUser moderatorClaims `shouldBe` Denied
+      prop "should not be able to update any user"
+        $ \uid -> updateUser moderatorClaims uid `shouldBe` Denied
+      it "should only be able to update himself" $ do
+        updateUser moderatorClaims (ucId moderatorClaims) `shouldBe` Granted
+      prop "should not be able to delete any user"
+        $ \uid -> deleteUser moderatorClaims uid `shouldBe` Denied
+      it "should only be able to delete himself" $ do
+        deleteUser moderatorClaims (ucId moderatorClaims) `shouldBe` Granted
+      it "should be able to get all users" $ do
+        getUsers moderatorClaims `shouldBe` Granted
+      it "should be able to get a user" $ do
+        getUser moderatorClaims `shouldBe` Granted
 
-      context "when user is Participant" $ do
-        it "should not be able to create new users" $ do
-          createUser participantClaims `shouldBe` Denied
-        prop "should not be able to update any user"
-          $ \uid -> updateUser participantClaims uid `shouldBe` Denied
-        it "should only be able to update himself" $ do
-          updateUser participantClaims (ucId participantClaims) `shouldBe` Granted
-        prop "should not be able to delete any user"
-          $ \uid -> deleteUser participantClaims uid `shouldBe` Denied
-        it "should only be able to delete himself" $ do
-          deleteUser participantClaims (ucId participantClaims) `shouldBe` Granted
-        it "should be able to get all users" $ do
-          getUsers participantClaims `shouldBe` Granted
-        it "should be able to get a user" $ do
-          getUser participantClaims `shouldBe` Granted
+    context "when user is Participant" $ do
+      it "should not be able to create new users" $ do
+        createUser participantClaims `shouldBe` Denied
+      prop "should not be able to update any user"
+        $ \uid -> updateUser participantClaims uid `shouldBe` Denied
+      it "should only be able to update himself" $ do
+        updateUser participantClaims (ucId participantClaims) `shouldBe` Granted
+      prop "should not be able to delete any user"
+        $ \uid -> deleteUser participantClaims uid `shouldBe` Denied
+      it "should only be able to delete himself" $ do
+        deleteUser participantClaims (ucId participantClaims) `shouldBe` Granted
+      it "should be able to get all users" $ do
+        getUsers participantClaims `shouldBe` Granted
+      it "should be able to get a user" $ do
+        getUser participantClaims `shouldBe` Granted
 
 
 createUser :: UserClaims -> Permission
